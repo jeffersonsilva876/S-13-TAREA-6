deaa014adb2dedd8db2888da6261cf8cd3b3f393
jefferson silva <85767459+jeffersonsilva876@users.noreply.github.com>
2022-03-10 12:39:30 -0500

    Add files via upload

---
+++ S13-TAREA_6-main/S13-TAREA_6/src/keys.js
@@ -0,0 +1,8 @@
+module.exports ={
+    database:{
+        host: `localhost`,
+        user: `root`,
+        password: `Nicolevm0350175238`,
+        database: `database_links`
+    }
+};
---
+++ S13-TAREA_6-main/S13-TAREA_6/src/routes/authentication.js
@@ -0,0 +1,34 @@
+const express = require(`express`);
+const router = express.Router();
+const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
+
+router.get(`/signup`, isNotLoggedIn, (req, res)=>{
+    res.render(`auth/signup`);
+});
+router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+// SINGIN
+router.get('/signin', isNotLoggedIn, (req, res) => {
+    res.render('auth/signin');
+});
+
+router.post('/signin', isNotLoggedIn, (req, res, next) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+    })(req, res, next);
+});
+router.get('/logout', isLoggedIn, (req, res) => {
+    req.logOut();
+    res.redirect('/');
+});
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile');
+});
+
+module.exports = router;
